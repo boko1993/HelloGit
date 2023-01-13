a36ad2dca71bcbd7f5edab1b019f0d272d24829b
boko1993 <121909801+boko1993@users.noreply.github.com>
2023-01-12 20:01:14 -0500

    Update GitDemo1.java
    edited

--- src/GitClasses/GitDemo1.java
+++ src/GitClasses/GitDemo1.java
@@ -4,8 +4,10 @@ public class GitDemo1 {
 
 	public static void main(String[] args) {
 System.out.println("Git i'm here");
-System.out.println("Java is fun");
-
+System.out.println("git is fun");
+System.out.println("git is fun");
+		System.out.println("git is fun");
+		System.out.println("git is fun");
 	}
 // hello from outside of the main method 
 }
