cce21252a7ac7fb8fb8bb5b608cfd742d08b0c96
boko1 <boko1@DESKTOP-KJE9GV6.myfiosgateway.com>
2023-01-12 19:18:35 -0500

    another print statment

--- src/GitClasses/GitDemo1.java
+++ src/GitClasses/GitDemo1.java
@@ -4,9 +4,9 @@ public class GitDemo1 {
 
 	public static void main(String[] args) {
 System.out.println("Git i'm here");
-
+System.out.println("Java is fun");
 
 
 	}
-
+// hello from outside of the main method 
 }
